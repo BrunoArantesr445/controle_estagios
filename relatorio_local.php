6e3060b704562fc9a9de4de57ac27c4b0069ebb0
bruno <brunogon542@gmail.com>
2024-10-29 10:04:36 -0300

    vai

--- relatorio_local.php
+++ relatorio_local.php
@@ -26,7 +26,7 @@
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             echo "<tr>";
             echo "<td>" . $row['id'] . "</td>";
-            echo "<td>" . $row['nome'] . "</td>";
+            echo "<td>" . $row['instituicao'] . "</td>";
             echo "<td>" . $row['observacao'] . "</td>";
             echo "</tr>";
         }
