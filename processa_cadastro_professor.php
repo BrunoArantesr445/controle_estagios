571e7ef5bbc771be5acf11f42034d0dffd1dc015
bruno <brunogon542@gmail.com>
2024-11-21 10:18:07 -0300

    professores

--- processa_cadastro_professor.php
+++ processa_cadastro_professor.php
@@ -7,19 +7,20 @@ $db = $database->getConnection();
 
 $professor = new Professor($db);
 
+// Atribuição dos valores recebidos via POST
 $professor->nome = $_POST['nome'];
 $professor->disponibilidade_horario = $_POST['disponibilidade_horario'];
 $professor->especialidade = $_POST['especialidade'];
 $professor->telefone = $_POST['telefone'];
-$professor->carga_horaria = $_POST['especialidade'];
+$professor->carga_horaria = $_POST['carga_horaria']; // Corrigido aqui para 'carga_horaria'
 
 if ($professor->create()) {
-    echo $msg = "Professor_cadastrado_com_sucesso.";   
-    header("location: index.php?txt=$msg");
+    $msg = "Professor cadastrado com sucesso.";  // Mensagem de sucesso
+    header("Location: index.php?txt=" . urlencode($msg)); // Redireciona com a mensagem
     exit(0);
 } else {
-    echo $msg = "Não_foi_possível_cadastrar_o_professor.";   
-    header("location: index.php?txt=$msg");
+    $msg = "Não foi possível cadastrar o professor.";  // Mensagem de erro
+    header("Location: index.php?txt=" . urlencode($msg)); // Redireciona com a mensagem
     exit(0);
 }
 ?>
